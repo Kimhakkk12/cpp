d700996ccf674b3a5d6e48d65a5d3c5da5c452f9
Kimhakkk12 <hongkimhakmlbb168@gmail.com>
2026-06-03 15:41:26 +0700

    first commit

---
+++ basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int age;
+    cout<<"Enter your age :";cin>>age;
+    if(age<=18){
+        cout<<"You so young";
+    }else{
+        cout<<"You so old";
+    }
+    return 0;
+}
